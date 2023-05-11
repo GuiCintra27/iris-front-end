6745fc682d25c0cf4d6b13c0caf2ffad2486078e
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-10 23:51:06 -0300

    feat: front-end sorting

---
+++ src/components/blog/searchBarSuggestion.js
@@ -0,0 +1,34 @@
+import SearchIcon from "../../assets/Icons/search-icon-gray.svg";
+import RecentIcon from "../../assets/Icons/recent-icon-gray.svg";
+import styled from "styled-components";
+
+export default function SearchBarSuggestion({ suggestionObject }) {
+    const { id, title, type } = suggestionObject;
+
+    return (
+        <StyledSuggestion>
+            <img src={type === "recent" ? RecentIcon : SearchIcon} alt="icon" />
+            <p>{title}</p>
+        </StyledSuggestion>
+    );
+}
+
+const StyledSuggestion = styled.div`
+    display: flex;
+    color: #7c7c7c;
+    align-items: center;
+    width: 100%;
+    height: 38px;
+    margin-left: -10px;
+
+    img {
+        margin-inline: 16px;
+        color: #7c7c7c;
+        width: 14px;
+        aspect-ratio: 1 / 1;
+        z-index: 3;
+    }
+    p {
+        color: black;
+    }
+`;
