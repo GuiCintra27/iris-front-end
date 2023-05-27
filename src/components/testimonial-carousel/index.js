5ee334068910ece1f1b29e5006e76e19eaf4f27f
Cintra <gui7lk@gmail.com>
2023-05-26 21:49:31 -0300

    fix: testimonials photos

--- src/components/testimonial-carousel/index.js
+++ src/components/testimonial-carousel/index.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import OpenQuotes from "../../assets/Icons/open-quotes.svg";
 import CloseQuotes from "../../assets/Icons/close-quotes.svg";
-import LumaPng from "../../assets/Fundadores/luma.png";
+import rodrigoPng from "../../assets/Testimonials/rodrigo.png";
+import driPng from "../../assets/Testimonials/dri.png";
+import jayPng from "../../assets/Testimonials/jay.png";
+import enioPng from "../../assets/Testimonials/enio.png";
+import enzoPng from "../../assets/Testimonials/enzo.png";
 import NextArrow from "../../assets/Icons/next-arrow.svg";
 import PreviousArrow from "../../assets/Icons/previous-arrow.svg";
 
@@ -46,27 +50,27 @@ const customIndicator = (onClickHandler, isSelected, index, label) => {
 export default function TestimonialCarousel() {
     const testimonialCards = [
         {
-            profilePicturePath: LumaPng,
+            profilePicturePath: rodrigoPng,
             name: "Rodrigo Borges",
             testimonial: "Quando me inscrevi no processo seletivo da Íris, minha motivação principal era me conhecer. Conhecer a história da comunidade a qual faço parte. De amar, me expressar e de ser quem eu sou. A Iniciativa me proporcionou tudo isso. Ela não apenas compartilhou parte de minha história, ela me mudou. E não digo apenas em um âmbito interpessoal. A Íris me ajudou a conquistar uma bolsa integral de intercâmbio na Índia, resultado das potentes reuniões com o pilar de Oportunidades. Aqui criei amizades com pessoas extraordinárias, que diariamente buscam e fazem a mudança. Hoje, para mim, a Íris é uma família.",
         },
         {
-            profilePicturePath: LumaPng,
+            profilePicturePath: driPng,
             name: "Andriély Fraga",
             testimonial: "Foi uma experiência incrível! Eu aprendi muito e consegui conhecer diversas pessoas. Além do voluntariado, a Íris me mostrou o verdadeiro significado de familia. Familia é o nosso laço mais querido, não precisa ter o mesmo sangue basta sentir o mesmo amor. O meu tempo na Íris acumulou os momentos mais felizes que vivenciei. Pude viver momentos de compartilhamento perfeitos com a comunidade ao mesmo tempo que aprendi muito com os colaboradores da Íris. Eu descreveria essa iniciativa como uma troca de objetivos e realidades que se encontram no final com o mesmo propósito: ser um lugar de conforto.",
         },
         {
-            profilePicturePath: LumaPng,
+            profilePicturePath: jayPng,
             name: "Jay Oliveira",
             testimonial: "As reuniões da Íris sempre foram muito receptivas e calorosas. Foram momentos para descontrair com temáticas super interessantes e atuais. Refletimos sobre nós mesmos e sobre os nossos amigos, pensando num futuro acolhedor e representativo. A comunidade Íris sempre vai estar guardada na minha memória. Foi uma experiência incrível!",
         },
         {
-            profilePicturePath: LumaPng,
+            profilePicturePath: enioPng,
             name: "Enio Ferreira",
             testimonial: "Fazer parte do corpo de voluntários da Iniciativa Íris foi uma das melhores coisas que já aconteceram comigo. Poder ajudar a construir esse projeto tão especial, que traz um enorme senso comunitário e um local seguro para tantos jovens LGBTQIA+ é uma honra. A todos que já passaram nesse breve tempo que estamos na ativa, é muito significativo sentir que todos ainda estão conectados como uma família. Só posso sentir orgulho e muito carinho por essa iniciativa e por tantas experiências que me proporcionou. Espero que esse projeto que me impactou como pessoa, venha crescer ainda mais e impactar mais jovens.",
         },
         {
-            profilePicturePath: LumaPng,
+            profilePicturePath: enzoPng,
             name: "Enzo Ferreira",
             testimonial: "A Iniciativa Íris é um pedaço de mim, uma verdadeira comunidade em que sempre há espaço para compartilhar medos, felicidades, angústias e vivências do dia de uma pessoa queer. Tenho certeza que qualquer pessoa que teve algum contato com a iniciativa tem um carinho pela entrega da equipe junto a interação da comunidade durantes estes dois primeiros ciclos.",
         },
@@ -83,6 +87,7 @@ export default function TestimonialCarousel() {
             autoPlay={true}
             interval={5000}
             showThumbs={false}
+            stopOnHover={true}
             swipeable={true}
             emulateTouch={true}
         >
@@ -127,8 +132,8 @@ const CarouselElement = styled.div`
         height: 124px;
         margin-left: 32px;
         img {
-            width: 60px;
-            height: 60px;
+            width: 75px;
+            height: 75px;
             border-radius: 50%;
         }
         p {
